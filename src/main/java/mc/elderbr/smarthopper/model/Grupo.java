794a750f5c1318fbdcb39a97e04eda5b569511b4
ElderBR <elderbarbosaromao@gmail.com>
2022-02-05 16:41:31 -0300

    Alterando as variaves para cdGrupo e dsGrupo e extendendo da tradução

--- src/main/java/mc/elderbr/smarthopper/model/Grupo.java
+++ src/main/java/mc/elderbr/smarthopper/model/Grupo.java
@@ -1,189 +1,26 @@
 package mc.elderbr.smarthopper.model;
 
-import mc.elderbr.smarthopper.interfaces.Dados;
-import mc.elderbr.smarthopper.interfaces.VGlobal;
-import mc.elderbr.smarthopper.utils.Msg;
-import mc.elderbr.smarthopper.utils.Utils;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.configuration.MemorySection;
-import org.bukkit.inventory.ItemStack;
+public class Grupo extends Traducao{
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.*;
-
-public class Grupo implements Dados {
-
-    private int id;
-    private String name;
-    private String lang;
+    private int cdGrupo;
+    private String dsGrupo;
 
     public Grupo() {
     }
 
-    @Override
-    public int getID() {
-        return id;
+    public int getCdGrupo() {
+        return cdGrupo;
     }
 
-    @Override
-    public void setID(int id) {
-        this.id = id;
+    public void setCdGrupo(int cdGrupo) {
+        this.cdGrupo = cdGrupo;
     }
 
-    @Override
-    public String getName() {
-        return name;
+    public String getDsGrupo() {
+        return dsGrupo;
     }
 
-    @Override
-    public void setName(String name) {
-        this.name = name;
+    public void setDsGrupo(String dsGrupo) {
+        this.dsGrupo = dsGrupo;
     }
-
-    @Override
-    public String lang() {
-        return lang;
-    }
-
-    @Override
-    public void setLang(String lang) {
-        this.lang = lang;
-    }
-
-    @Override
-    public String traducao() {
-        return null;
-    }
-
-    @Override
-    public void setTraducao(String traducao) {
-
-    }
-
-
-    public boolean isItemGrupo(String material) {
-
-        List<String> list = new ArrayList<>();
-        switch (getName()) {
-            case "stone":
-                list.add("cobble");
-                list.add("sandstone");
-                list.add("redstone");
-                list.add("blackstone");
-                list.add("glowstone");
-                list.add("brick");
-                list.add("bricks");
-                list.add("grindstone");
-                list.add("end stone");
-                list.add("lodestone");
-                list.add("stonecutter");
-                break;
-            case "oak":
-                list.add("dark oak");
-                break;
-            case "blue":
-                list.add("light blue");
-                break;
-            case "gray":
-                list.add("light gray");
-                break;
-            case "sand":
-                list.add("sandstone");
-                break;
-            case "sandstone":
-                list.add("red sandstone");
-                break;
-            case "egg":
-                list.add("leggings");
-                break;
-        }
-        for (String items : list) {
-            if (material.contains(items)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public void createGrupos() {
-        List<String> grupoList = new ArrayList<>();
-        File file = new File(VGlobal.ARQUIVO, "grupo_names.txt");
-
-        List<String> grupoNot = new ArrayList<>();
-        grupoNot.add("on");
-        grupoNot.add("a");
-        grupoNot.add("o");
-        grupoNot.add("of");
-        grupoNot.add("the");
-        grupoNot.add("and");
-
-        Map<String, String> grupoMap = new HashMap<>();
-
-        for (Material materials : Material.values()) {
-            if (materials.isItem() && !materials.isAir()) {
-                for (String grupo : Utils.ToMaterial(materials).split("\\s")) {
-                    if (grupoNot.contains(grupo)) continue;
-                    if (!grupoList.contains(grupo)) {
-                        grupoList.add(grupo);
-                        grupoMap.put(grupo, VGlobal.ITEM_LANG_MAP.get(Utils.ToMaterial(materials)).get("pt_br"));
-                    }
-                }
-            }
-        }
-
-        grupoList = new ArrayList<>(grupoMap.keySet());
-        Collections.sort(grupoList);
-
-        TreeMap<String, String> map = new TreeMap<>();
-        map.putAll(grupoMap);
-
-        try (BufferedWriter escrever = Files.newBufferedWriter(file.toPath(), StandardCharsets.UTF_8)) {
-            for (Map.Entry<String, String> grupMap : map.entrySet()) {
-                escrever.write(grupMap.getKey().concat(": ").concat(Utils.ToUTF(grupMap.getValue())));
-                escrever.newLine();
-                escrever.flush();
-            }
-        } catch (IOException e) {
-            Msg.ServidorErro(e, "createGrupos()", getClass());
-        }
-    }
-
-    public static int newID() {
-        int newId = 0;
-        for (int id : VGlobal.GRUPO_MAP_ID.keySet()) {
-            if (id > newId) {
-                newId = id;
-            }
-        }
-        return (newId + 1);
-    }
-
-    /***
-     * Percorre o nome do item verifica se contém espaço se sim compara os nomes separados
-     * @param item nome do item
-     * @return falso ou verdadeiro
-     */
-    public boolean isContentItem(String item) {
-        // Se o nome do grupo conter mais do que nome
-        if (this.name.split("\\s").length > 1) {
-            if (item.contains(name)) {
-                return true;
-            }
-        } else {
-            //Percorre o nome do item e ver se o nome do grupo existe
-            for (String nameItem : item.split("\\s")) {
-                if (nameItem.equals(this.name)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 }
