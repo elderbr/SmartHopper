1fa97991392a9b3d574a34106e6485acddeb6583
ElderBR <elderbarbosaromao@gmail.com>
2022-02-05 15:42:40 -0300

    Lang é o tipo de languigem do player (jogador)

---
+++ src/main/java/mc/elderbr/smarthopper/model/Lang.java
@@ -0,0 +1,26 @@
+package mc.elderbr.smarthopper.model;
+
+public class Lang {
+
+    private int cdLang;
+    private String dsLang;
+
+    public Lang() {
+    }
+
+    public int getCdLang() {
+        return cdLang;
+    }
+
+    public void setCdLang(int cdLang) {
+        this.cdLang = cdLang;
+    }
+
+    public String getDsLang() {
+        return dsLang;
+    }
+
+    public void setDsLang(String dsLang) {
+        this.dsLang = dsLang;
+    }
+}
