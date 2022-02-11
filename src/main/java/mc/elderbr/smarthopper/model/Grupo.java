4c1711adbfa171e9f66c1144510c58f40e27e0d8
ElderBR <elderbarbosaromao@gmail.com>
2022-02-11 11:16:20 -0300

    Adicionado lista de item ao grupo com get e set

--- src/main/java/mc/elderbr/smarthopper/model/Grupo.java
+++ src/main/java/mc/elderbr/smarthopper/model/Grupo.java
@@ -1,9 +1,12 @@
 package mc.elderbr.smarthopper.model;
 
+import java.util.List;
+
 public class Grupo extends Traducao{
 
     private int cdGrupo;
     private String dsGrupo;
+    private List<Item> listItem;
 
     public Grupo() {
     }
@@ -22,5 +25,13 @@ public class Grupo extends Traducao{
 
     public void setDsGrupo(String dsGrupo) {
         this.dsGrupo = dsGrupo;
+    }
+
+    public List<Item> getListItem() {
+        return listItem;
+    }
+
+    public void setListItem(List<Item> listItem) {
+        this.listItem = listItem;
     }
 }
