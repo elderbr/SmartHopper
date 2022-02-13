5e10099c76c67a385a31d258ad21022392dcc3de
ElderBR <elderbarbosaromao@gmail.com>
2022-02-12 22:54:53 -0300

    Procura na tabela grupo do item o código do item com e sem a tradução

--- src/main/java/mc/elderbr/smarthopper/cmd/GrupoComando.java
+++ src/main/java/mc/elderbr/smarthopper/cmd/GrupoComando.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GrupoComando implements CommandExecutor {
@@ -24,6 +25,7 @@ public class GrupoComando implements CommandExecutor {
     private ItemStack itemStack;
 
     // Grupo
+    private int cdGrupo;
     private Grupo grupo;
     private GrupoDao grupoDao;
     private List<Grupo> listGrupo;
@@ -63,19 +65,22 @@ public class GrupoComando implements CommandExecutor {
                     item = VGlobal.ITEM_NAME_MAP.get(Utils.toItem(itemStack));
                     item.setDsLang(langPlayer);
                     listGrupo = grupoDao.selectListGrupo(item);
-                    if(listGrupo.size()>1) {
-                        for (Grupo gp : listGrupo) {
+                    if (listGrupo.size() > 1) {
+                        listGrupo.forEach(gp->{
                             Msg.GrupoPlayer(player, gp);
+                        });
+                    } else {
+                        if (!listGrupo.isEmpty()) {
+                            Msg.GrupoPlayer(player, listGrupo.get(0));
+                        } else {
+                            Msg.PlayerRed(player, "Grupo não encontrado!!!");
                         }
-                    }else{
-                        Msg.GrupoPlayer(player, listGrupo.get(0));
                     }
                     return false;
                 }
                 if (grupo != null) {
                     Msg.GrupoPlayer(player, grupo);
-                }
-                else {
+                } else {
                     Msg.PlayerRed(player, "Grupo não encontrado!!!");
                 }
                 return false;
@@ -84,17 +89,19 @@ public class GrupoComando implements CommandExecutor {
             if (command.getName().equalsIgnoreCase("traducaoGrupo")) {
                 if (cmd.length() > 0) {
                     grupo = new Grupo();
-                    grupo.setDsGrupo(cmd);
+                    getIdTraducao(args);
                     grupo.setDsLang(langPlayer);
                     grupo = grupoDao.select(grupo);
-                    if (grupo == null) {
-                        grupo.setDsTraducao(cmd);
-                        if (traducaoDao.insert(grupo)>0)
+                    if (grupo.getDsTraducao() == null) {
+                        getIdTraducao(args);
+                        grupo.setCdLang(VGlobal.LANG_NAME_MAP.get(langPlayer).getCdLang());
+                        if (traducaoDao.insert(grupo) > 0) {
                             Msg.PlayerGold(player, "Tradução para o grupo ".concat(grupo.getDsGrupo()).concat(" adicionado com sucesso!!!"));
-                        else
+                        } else {
                             Msg.PlayerGold(player, "Erro ao adicionar a tradução para o ".concat(grupo.getDsGrupo()).concat("!!!"));
+                        }
                     } else {
-                        grupo.setDsTraducao(cmd);
+                        getIdTraducao(args);
                         if (traducaoDao.update(grupo) > 0)
                             Msg.PlayerGold(player, "Tradução para o grupo ".concat(grupo.getDsGrupo()).concat(" atualizado com sucesso!!!"));
                         else
@@ -109,4 +116,28 @@ public class GrupoComando implements CommandExecutor {
 
         return false;
     }
+
+    private boolean getIdTraducao(String[] args) {
+        cmd = Utils.NAME_ARRAY(args);
+        if (args.length > 1) {// 23 Barra de ferro
+            try {
+                cdGrupo = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                Msg.PlayerRed(player, "Digite o código do grupo antes da sua tradução!!!");
+                cdGrupo = 0;
+            }
+
+            StringBuilder listName = new StringBuilder();
+            if (cdGrupo > 0) {
+                for (int i = 1; i < args.length; i++) {
+                    listName.append(args[i] + " ");
+                }
+            }
+            grupo.setDsTraducao(listName.toString().trim());
+            grupo.setCdGrupo(cdGrupo);
+            return true;
+        }
+        return false;
+    }
+
 }
