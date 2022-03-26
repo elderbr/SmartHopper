5c57f18bb5cc69b298a99ab1c189f2d7c6db4719
ElderBR <elderbarbosaromao@gmail.com>
2022-03-26 16:42:28 -0300

    Criando a classe Lang

---
+++ src/main/java/mc/elderbr/smarthopper/model/Lang.java
@@ -0,0 +1,30 @@
+package mc.elderbr.smarthopper.model;
+
+public class Lang {
+    private int cdLang;
+    private String dsLang;
+
+    public Lang() {
+    }
+
+    public int getCdLang() {
+        return cdLang;
+    }
+
+    public void setCdLang(int cdLang) {
+        this.cdLang = cdLang;
+    }
+
+    public String getDsLang() {
+        return dsLang;
+    }
+
+    public void setDsLang(String dsLang) {
+        this.dsLang = dsLang;
+    }
+
+    @Override
+    public String toString() {
+        return dsLang;
+    }
+}
