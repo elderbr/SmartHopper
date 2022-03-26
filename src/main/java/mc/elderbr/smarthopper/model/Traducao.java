0c4e00a726956e7dcf80eb77ef12ed3ec8afd8b6
ElderBR <elderbarbosaromao@gmail.com>
2022-03-26 16:43:17 -0300

    Tradução extende de Lang

--- src/main/java/mc/elderbr/smarthopper/model/Traducao.java
+++ src/main/java/mc/elderbr/smarthopper/model/Traducao.java
@@ -1,51 +1,27 @@
 package mc.elderbr.smarthopper.model;
 
-import mc.elderbr.smarthopper.interfaces.VGlobal;
-import mc.elderbr.smarthopper.utils.Msg;
+public class Traducao extends Lang {
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+    private int cdTraducao;
+    private String dsTraducao;
 
-public class Traducao {
-
-    private String item = null;
-    private Map<String, String> langMap = new HashMap<>();
 
     public Traducao() {
     }
 
-    public String getItem() {
-        return item;
+    public int getCdTraducao() {
+        return cdTraducao;
     }
 
-    public void setItem(String item) {
-        this.item = item;
+    public void setCdTraducao(int cdTraducao) {
+        this.cdTraducao = cdTraducao;
     }
 
-    public void addItemTraducao(String langKey, String traducao) {
-        if (VGlobal.ITEM_LANG_MAP.get(item) == null) {
-            langMap.put(langKey, traducao);
-            VGlobal.ITEM_LANG_MAP.put(item, langMap);
-        } else {
-            VGlobal.ITEM_LANG_MAP.get(item).put(langKey, traducao);
-        }
+    public String getDsTraducao() {
+        return dsTraducao;
     }
 
-    public void addItemTraducao(String langKey, Object traducao) {
-        if (VGlobal.ITEM_LANG_MAP.get(item) == null) {
-            langMap.put(langKey, String.valueOf(traducao));
-            VGlobal.ITEM_LANG_MAP.put(item, langMap);
-        } else {
-            VGlobal.ITEM_LANG_MAP.get(item).put(langKey, String.valueOf(traducao));
-        }
+    public void setDsTraducao(String dsTraducao) {
+        this.dsTraducao = dsTraducao;
     }
-
-
 }
