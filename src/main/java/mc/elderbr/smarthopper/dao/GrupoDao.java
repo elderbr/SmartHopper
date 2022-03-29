4ae8714fe8ccbdfc48393200be98f287bc036818
ElderBR <elderbarbosaromao@gmail.com>
2022-03-29 18:43:31 -0300

    Adicionado classe GrupoDao, metodo para selecionar todos os grupos com itens e tradução

---
+++ src/main/java/mc/elderbr/smarthopper/dao/GrupoDao.java
@@ -0,0 +1,43 @@
+package mc.elderbr.smarthopper.dao;
+
+import mc.elderbr.smarthopper.model.Grupo;
+import mc.elderbr.smarthopper.utils.Msg;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GrupoDao {
+
+    public GrupoDao() {
+    }
+
+    public static void SELECT_ALL(){
+        Grupo grupo = null;
+        try {
+           PreparedStatement stm = Conexao.repared("SELECT * FROM grupo g " +
+                   "LEFT JOIN traducao t ON t.cdGrupo = g.cdGrupo " +
+                   "LEFT JOIN lang l ON l.cdLang = t.cdLang");
+            ResultSet rs = stm.executeQuery();
+
+            if(rs.next()){
+                grupo = new Grupo();
+                grupo.setCdGrupo(rs.getInt("cdGrupo"));
+                grupo.setDsGrupo(rs.getString("dsGrupo"));
+            }
+
+            while(rs.next()){
+                if(!grupo.getDsGrupo().equals(rs.getString("dsGrupo"))){
+                    grupo = new Grupo();
+                    grupo.setCdGrupo(rs.getInt("cdGrupo"));
+                    grupo.setDsGrupo(rs.getString("dsGrupo"));
+                }
+                grupo.addTraducao(rs.getString("dsLang"), rs.getString("dsTraducao"));
+            }
+        }catch (SQLException e){
+            Msg.ServidorErro("Erro ao selecionar todos os grupos!!!", "SELECT_ALL", GrupoDao.class, e);
+        }finally {
+            Conexao.desconect();
+        }
+    }
+}
