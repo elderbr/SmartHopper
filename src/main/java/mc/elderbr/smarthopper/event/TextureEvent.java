1ba910aa09b3d4b7c7cb9b7e9957c240bf58d9dd
ElderBR <elderbarbosaromao@gmail.com>
2022-10-01 21:46:44 -0300

    Adicionando prioridade para o carregamento da textura

--- src/main/java/mc/elderbr/smarthopper/event/TextureEvent.java
+++ src/main/java/mc/elderbr/smarthopper/event/TextureEvent.java
@@ -1,16 +1,20 @@
 package mc.elderbr.smarthopper.event;
 
+import mc.elderbr.smarthopper.utils.Msg;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerResourcePackStatusEvent;
 
 public class TextureEvent implements Listener {
 
     // http://elderbr.com/minecraft/textures/SmartHopper.zip
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onJoin(PlayerJoinEvent event){
         event.getPlayer().setResourcePack("http://elderbr.com/minecraft/textures/SmartHopper.zip");
     }
-
 }
