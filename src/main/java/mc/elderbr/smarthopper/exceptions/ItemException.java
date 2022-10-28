8b10e468cc30ab90ca82a65e5e8dd1f06198f097
ElderBR <elderbarbosaromao@gmail.com>
2022-10-28 00:08:50 -0300

    Organizando a classe exception dos itens

--- src/main/java/mc/elderbr/smarthopper/exceptions/ItemException.java
+++ src/main/java/mc/elderbr/smarthopper/exceptions/ItemException.java
@@ -3,13 +3,11 @@ package mc.elderbr.smarthopper.exceptions;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 
-public class ItemException extends RuntimeException{
-
-    private Block myBlock;
+public class ItemException extends RuntimeException {
     private String world, msg;
     private int x, y, z;
 
-    private String pularLinha = "=====================================================\n";
+    private final String pularLinha = "=====================================================\n";
 
 
     public ItemException() {
@@ -20,19 +18,14 @@ public class ItemException extends RuntimeException{
     }
 
     public ItemException(String message, Block block) throws Exception {
-        myBlock = block;
         world = block.getWorld().getName();
         x = block.getLocation().getBlockX();
         y = block.getLocation().getBlockY();
         z = block.getLocation().getBlockZ();
 
-        msg = pularLinha+message+"\nHopper localizado no mundo "+ world + " x: "+ x+" | y: "+ y+" | z: "+ z;
+        msg = pularLinha + message + "\nHopper localizado no mundo " + world + " x: " + x + " | y: " + y + " | z: " + z;
 
         Bukkit.getServer().getConsoleSender().sendMessage(msg);
         throw new Exception(msg);
-    }
-
-    public void setMessage(String message){
-
     }
 }
