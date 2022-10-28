ee7cd93db3951fc2642fc6a7c5cbe2519f8aeb8a
ElderBR <elderbarbosaromao@gmail.com>
2022-10-28 00:07:14 -0300

    Classe para tratar erros dos itens

---
+++ src/main/java/mc/elderbr/smarthopper/exceptions/ItemException.java
@@ -0,0 +1,38 @@
+package mc.elderbr.smarthopper.exceptions;
+
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+
+public class ItemException extends RuntimeException{
+
+    private Block myBlock;
+    private String world, msg;
+    private int x, y, z;
+
+    private String pularLinha = "=====================================================\n";
+
+
+    public ItemException() {
+    }
+
+    public ItemException(String message) {
+        super(message);
+    }
+
+    public ItemException(String message, Block block) throws Exception {
+        myBlock = block;
+        world = block.getWorld().getName();
+        x = block.getLocation().getBlockX();
+        y = block.getLocation().getBlockY();
+        z = block.getLocation().getBlockZ();
+
+        msg = pularLinha+message+"\nHopper localizado no mundo "+ world + " x: "+ x+" | y: "+ y+" | z: "+ z;
+
+        Bukkit.getServer().getConsoleSender().sendMessage(msg);
+        throw new Exception(msg);
+    }
+
+    public void setMessage(String message){
+
+    }
+}
