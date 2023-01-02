d4e14921c42b368e08182c36cce1d8f4907a6a64
ElderBR <elderbarbosaromao@gmail.com>
2023-01-02 19:19:28 -0300

    Refaturado a classe item

--- src/main/java/mc/elderbr/smarthopper/model/Item.java
+++ src/main/java/mc/elderbr/smarthopper/model/Item.java
@@ -1,56 +1,27 @@
 package mc.elderbr.smarthopper.model;
 
-import mc.elderbr.smarthopper.interfaces.Funil;
-import mc.elderbr.smarthopper.interfaces.VGlobal;
-import mc.elderbr.smarthopper.utils.Msg;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.EnchantmentStorageMeta;
-import org.bukkit.inventory.meta.PotionMeta;
-import org.bukkit.potion.PotionData;
-import org.bukkit.potion.PotionType;
-import org.jetbrains.annotations.NotNull;
+import mc.elderbr.smarthopper.abstracts.Funil;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import static mc.elderbr.smarthopper.interfaces.VGlobal.ITEM_MAP_NAME;
+public class Item extends Funil {
 
-public class Item implements Funil {
-
-    private int codigo = 0;
+    private int id = 0;
     private String name;
-    private boolean bloqueado = false;
-    private Map<String, String> traducao = new HashMap<>();
+    private boolean blocked = false;
 
-    private ItemStack itemStack;
-    private final String enchanted_book = "enchanted book";
-
-    public Item() {
-    }
-
-    public Item(ItemStack itemStack) {
-        name = toItemStack(itemStack);
-        this.itemStack = itemStack;
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
+    private List<Grupo> listGrupo = new ArrayList<>();
 
     @Override
-    public Funil setCodigo(int codigo) {
-        this.codigo = codigo;
+    public Funil setId(int id) {
+        this.id = id;
         return this;
     }
 
     @Override
-    public int getCodigo() {
-        return codigo;
+    public int getId() {
+        return id;
     }
 
     @Override
@@ -65,144 +36,27 @@ public class Item implements Funil {
     }
 
     @Override
-    public boolean isBloqueado() {
-        return bloqueado;
-    }
-
-    @Override
-    public Funil setBloqueado(boolean bloqueado) {
-        this.bloqueado = bloqueado;
+    public Funil setBlocked(boolean blocked) {
+        this.blocked = blocked;
         return this;
     }
 
     @Override
-    public Map<String, String> getTraducao() {
-        return traducao;
+    public boolean isBlocked() {
+        return blocked;
     }
 
-    public String toItemStack(ItemStack itemStack) {
-        return itemStack.getType().getKey().getKey().toLowerCase().replaceAll("_", " ");
-    }
-
-    public ItemStack parseItemStack() {
-        itemStack = null;
-        if (name.contains("potion")) {
-            String potion = null;
-            if (name.contains("lingering")) {
-                potion = name.replaceAll("lingering potion", "").trim();
-                itemStack = new ItemStack(Material.LINGERING_POTION);
-            } else if (name.contains("splash")) {
-                potion = name.replaceAll("splash potion", "").trim();
-                itemStack = new ItemStack(Material.SPLASH_POTION);
-            } else {
-                potion = name.replaceAll("potion", "").trim();
-                itemStack = new ItemStack(Material.POTION);
-            }
-
-            if (!potion.isEmpty()) {
-                PotionMeta meta = (PotionMeta) itemStack.getItemMeta();
-                meta.setBasePotionData(new PotionData(PotionType.valueOf(potion.replaceAll("\\s", "_").toUpperCase())));
-                itemStack.setItemMeta(meta);
-            }
-            return itemStack;
-        }
-        if (name.contains(enchanted_book)) {
-            itemStack = new ItemStack(Material.ENCHANTED_BOOK);
-            //
-            String encantamento = getName().replaceAll(enchanted_book, "").trim();
-            NamespacedKey key = null;
-            for (Enchantment enchantment : Enchantment.values()) {
-                String nameEnch = enchantment.getKey().getKey().replaceAll("_", " ").toLowerCase();
-                if (encantamento.equalsIgnoreCase(nameEnch)) {
-                    key = enchantment.getKey();
-                }
-            }
-            EnchantmentStorageMeta meta = (EnchantmentStorageMeta) itemStack.getItemMeta();
-            meta.addStoredEnchant(Enchantment.getByKey(key), 1, true);
-            itemStack.setItemMeta(meta);
-            return itemStack;
-        }
-        return Item.ParseItemStack(name);
-    }
-
-    public ItemStack parseItemStack(String name) {
-        return new ItemStack(Material.valueOf(name.toUpperCase().replaceAll("\\s", "_")));
-    }
-
-    public static ItemStack ParseItemStack(String name) {
-        return new ItemStack(Material.valueOf(name.toUpperCase().replaceAll("\\s", "_")));
-    }
-
-    public Item parse(){
-        switch (itemStack.getType()){
-            case POTION:
-            case LINGERING_POTION:
-            case SPLASH_POTION:
-                PotionMeta meta = null;
-                String potionName = null;
-                if(itemStack.getItemMeta()!=null){
-                    meta = (PotionMeta) itemStack.getItemMeta();
-                    potionName = meta.getBasePotionData().getType().name();
-                }
-                name = toItemStack(itemStack)+" ".concat(potionName.replaceAll("_"," ").toLowerCase());
-                break;
-            case ENCHANTED_BOOK:
-                EnchantmentStorageMeta metaBock = (EnchantmentStorageMeta) itemStack.getItemMeta();
-                for(Enchantment enchantment : metaBock.getStoredEnchants().keySet()){
-                    name = enchanted_book+" "+enchantment.getKey().getKey().toLowerCase().replaceAll("_"," ");
-                }
-                break;
-            default:
-                name = toItemStack(itemStack);
-        }
+    public Item addListGrupo(Grupo grupo) {
+        listGrupo.add(grupo);
         return this;
     }
 
-    public static Item PARSE(ItemStack itemStackInventory) {
-        return ITEM_MAP_NAME.get(itemStackInventory.getType().getKey().getKey().toLowerCase().replaceAll("_", " "));
+    public Item removeListGrupo(Grupo grupo) {
+        listGrupo.remove(grupo);
+        return this;
     }
 
-    public static String ToName(ItemStack itemStack) {
-        return itemStack.getType().getKey().getKey().toLowerCase().replaceAll("_", " ");
-    }
-
-    public static void CreateItem() {
-        for (Material m : Material.values()) {
-            ItemStack itemStack = new ItemStack(m);
-            if (itemStack != null && itemStack.getType() != Material.AIR && itemStack.getType().isItem()) {
-                Item item = new Item(itemStack);
-                if (!VGlobal.ITEM_NAME_LIST.contains(item.getName())) {
-                    VGlobal.ITEM_NAME_LIST.add(item.getName());
-                }
-            }
-        }
-
-        Pocao.Create();// POÇÕES E SEU EFEITOS
-        LivroEncantado.Create();// LIVRO ENCANTADOS
-
-        Collections.sort(VGlobal.ITEM_NAME_LIST);// Organizando em ordem alfabetica
-
-        // Criando itens com código e nome e adicionando na lista global
-        int cod = 1;
-        for (String name : VGlobal.ITEM_NAME_LIST) {
-            Item item = new Item();
-            item.setCodigo(cod);
-            item.setName(name);
-            VGlobal.ITEM_LIST.add(item);
-            cod++;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Item:{" +
-                "codigo:" + codigo +
-                ", name:'" + name + '\'' +
-                ", bloqueado:" + bloqueado +
-                '}';
-    }
-
-    public boolean equals(@NotNull Item item) {
-        return (codigo == item.getCodigo());
+    public List<Grupo> getListGrupo() {
+        return listGrupo;
     }
 }
