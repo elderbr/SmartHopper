6963bc14490f88d5e0c7da3e36122bc964e39857
ElderBR <elderbarbosaromao@gmail.com>
2024-08-17 18:26:01 -0300

    Criação do controle do arquivo de configuração

--- src/main/java/mc/elderbr/smarthopper/controllers/ConfigController.java
+++ src/main/java/mc/elderbr/smarthopper/controllers/ConfigController.java
@@ -1,2 +1,106 @@
-package mc.elderbr.smarthopper.controllers;public class ConfigController {
+package mc.elderbr.smarthopper.controllers;
+
+import mc.elderbr.smarthopper.dao.ConfigDao;
+import mc.elderbr.smarthopper.exceptions.ConfigException;
+import mc.elderbr.smarthopper.interfaces.VGlobal;
+import mc.elderbr.smarthopper.utils.Msg;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class ConfigController implements VGlobal {
+
+    private ConfigDao dao = new ConfigDao();
+
+    public ConfigController() {
+    }
+
+    public boolean isAdm(Player player) {
+        if (player == null) {
+            throw new ConfigException("Não existe um player para comparar!");
+        }
+        return dao.containsAdm(player);
+    }
+
+    public int getVersion() {
+        if (dao.findByVersion() < VERSION_INT) {
+            dao.saveVersion();
+        }
+        return dao.findByVersion();
+    }
+
+    public String getTutorial() {
+        String tutorial = dao.findByTutorial();
+        if (tutorial == null || tutorial.isBlank()) {
+            dao.saveTutorial();
+        }
+        return dao.findByTutorial();
+    }
+
+    public boolean saveUseTexture(Object isUseTexture) {
+        try {
+            if (isUseTexture instanceof Boolean useTexture) {
+                dao.saveUseTexture(useTexture);
+                return true;
+            }
+            if (isUseTexture instanceof String useTexture) {
+                boolean use = true;
+                if (useTexture.equalsIgnoreCase("true") || useTexture.equalsIgnoreCase("false")) {
+                    use = Boolean.getBoolean(useTexture);
+                }
+                dao.saveUseTexture(use);
+                return true;
+            }
+        } catch (Exception e) {
+            throw new ConfigException(e.getMessage());
+        }
+        return false;
+    }
+
+    public String getTexture() {
+        try {
+            return dao.findByTexture();
+        } catch (ConfigException e) {
+            dao.saveTexture();
+        }
+        return dao.findByTexture();
+    }
+
+    public boolean getUseTexture() {
+        try {
+            return dao.findByUseTexture();
+        } catch (ConfigException e) {
+            dao.saveUseTexture();
+        }
+        return dao.findByUseTexture();
+    }
+
+    public void reset(){
+        dao.saveAuthor();
+        dao.saveDiscord();
+        dao.saveVersion();
+        dao.saveTexture();
+        dao.saveUseTexture();
+        dao.saveAdm();
+    }
+
+    public static void RESET() {
+        ConfigController ctrl = new ConfigController();
+        ConfigDao dao = new ConfigDao();
+        try {
+
+            List<String> list = new AdmController().getAdms();
+
+            if (dao.findByVersion() < VERSION_INT) {
+                dao.saveAuthor();
+                dao.saveDiscord();
+                dao.saveVersion();
+                dao.saveTutorial();
+                dao.saveTexture();
+                ctrl.getUseTexture();
+            }
+        } catch (ConfigException e) {
+            Msg.ServidorGold(e.getMessage());
+        }
+    }
 }
