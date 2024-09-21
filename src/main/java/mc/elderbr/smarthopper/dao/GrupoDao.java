5ce6a0ebff3bc2af993eb2b83d75f1923d0baaa9
ElderBR <elderbarbosaromao@gmail.com>
2024-09-21 18:30:40 -0300

    Removendo classe desnecessárias

--- src/main/java/mc/elderbr/smarthopper/dao/GrupoDao.java
+++ src/main/java/mc/elderbr/smarthopper/dao/GrupoDao.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class GrupoDao implements VGlobal {
 
