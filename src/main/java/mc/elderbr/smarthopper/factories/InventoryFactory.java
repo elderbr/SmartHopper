f7ca5b7e464b89d0a7103853ea526675609f8527
ElderBR <elderbarbosaromao@gmail.com>
2025-05-20 22:30:20 -0300

    InventoryFactory: Criado o método para que retorna um Inventory para configuração dos itens no Smart Hopper

--- src/main/java/mc/elderbr/smarthopper/factories/InventoryFactory.java
+++ src/main/java/mc/elderbr/smarthopper/factories/InventoryFactory.java
@@ -1,4 +1,33 @@
 package mc.elderbr.smarthopper.factories;
 
-public class InventoryFactory {
+import mc.elderbr.smarthopper.interfaces.Botao;
+import mc.elderbr.smarthopper.interfaces.VGlobal;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
+
+public class InventoryFactory implements Botao, VGlobal {
+
+    public static InventoryFactory instance;
+
+    private InventoryFactory() {
+    }
+
+    public static InventoryFactory getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new InventoryFactory();
+        }
+        return instance;
+    }
+
+    public Inventory InventoryConfigurationHopper(){
+        Inventory inventory = Bukkit.createInventory(null, 18, NAME_RECIPE);
+        for(int i = 10; i < 17; i++){
+            inventory.setItem(i, BtnBlocked());
+        }
+        inventory.setItem(17, BtnSalva());
+        return inventory;
+    }
 }
