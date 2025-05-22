2af4abd1b1afeae3d6edeee3b2bf133868d97d4b
ElderBR <elderbarbosaromao@gmail.com>
2025-05-22 14:47:55 -0300

    ITitle: Refaturando o estilo dos títulos

--- src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
+++ src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
@@ -2,11 +2,11 @@ package mc.elderbr.smarthopper.interfaces;
 
 public interface ITitle {
 
-    String TITLE_ID_ITEM = "§e§lID: ";
-    String TITLE_NAME_ITEM = "§2§lItem: §6";
+    String TITLE_ID_ITEM = "§eID: ";
+    String TITLE_NAME_ITEM = "§2Item: §6";
     String TITLE_GROUP = "§lGrupo: §r";
-    String TITLE_ID_GROUP = "§6§lID: ";
-    String TITLE_NAME_GROUP = "§9§lGrupo: §e";
+    String TITLE_ID_GROUP = "§6ID: ";
+    String TITLE_NAME_GROUP = "§9Grupo: §r";
     String TITLE_NEW_GROUP = "§lNovo Grupo: §r";
 
     String NAME_SMART_HOPPER = "§f§lSmart Hopper";
