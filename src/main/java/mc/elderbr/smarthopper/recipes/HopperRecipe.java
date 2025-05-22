a3bdb70ee040dd795c1c6ffdf8aa317d92dac7b3
ElderBR <elderbarbosaromao@gmail.com>
2025-05-22 14:49:53 -0300

    HopperRecipe: Refaturando a criação do funil de ferro do Smart Hopper

--- src/main/java/mc/elderbr/smarthopper/recipes/HopperRecipe.java
+++ src/main/java/mc/elderbr/smarthopper/recipes/HopperRecipe.java
@@ -1,5 +1,6 @@
 package mc.elderbr.smarthopper.recipes;
 
+import mc.elderbr.smarthopper.factories.ItemFactory;
 import mc.elderbr.smarthopper.interfaces.VGlobal;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -8,13 +9,12 @@ import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class HopperRecipe implements VGlobal {
 
     private static ShapedRecipe recipe;
-    public static ItemStack SmartHopper = new ItemStack(Material.HOPPER);
+    public static ItemStack hopper = new ItemStack(Material.HOPPER);
     private static ItemMeta meta;
     private static List<String> lore = new ArrayList<>();
 
@@ -22,12 +22,7 @@ public class HopperRecipe implements VGlobal {
     }
 
     public static ShapedRecipe createSmartHopper() {
-        SmartHopper = new ItemStack(Material.HOPPER);
-        meta = SmartHopper.getItemMeta();
-        meta.setDisplayName(TITLE_RECIPE);
-        meta.setLore(Arrays.asList(new String[]{NAME_RECIPE}));
-        SmartHopper.setItemMeta(meta);
-        recipe = new ShapedRecipe(new NamespacedKey(SMARTHOPPER, "iron_smart_hopper"), SmartHopper);
+        recipe = new ShapedRecipe(new NamespacedKey(SMARTHOPPER, "iron_smart_hopper"), ItemFactory.getIronHopper());
         recipe.setGroup("minecraft:redstone");
         recipe.shape(new String[]{"%B%", "%C%", " % "});
         recipe.setIngredient('B', Material.IRON_BARS);
