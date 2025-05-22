23b17ceb27a59bf5bd21f80972e6608803bf0844
ElderBR <elderbarbosaromao@gmail.com>
2025-05-22 20:04:35 -0300

    ITitle: Alterado a formatação das cores dos titulos

--- src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
+++ src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
@@ -2,11 +2,11 @@ package mc.elderbr.smarthopper.interfaces;
 
 public interface ITitle {
 
-    String TITLE_ID_ITEM = "§eID: ";
-    String TITLE_NAME_ITEM = "§2Item: §6";
-    String TITLE_GROUP = "§lGrupo: §r";
-    String TITLE_ID_GROUP = "§6ID: ";
-    String TITLE_NAME_GROUP = "§9Grupo: §r";
+    String TITLE_ID_ITEM = "§eID: §f";
+    String TITLE_NAME_ITEM = "§2§lItem: §r§f";
+    String TITLE_GROUP = "§lGrupo: §f";
+    String TITLE_ID_GROUP = "§eID: §f";
+    String TITLE_NAME_GROUP = "§9§lGrupo: §r§f";
     String TITLE_NEW_GROUP = "§lNovo Grupo: §r";
 
     String NAME_SMART_HOPPER = "§f§lSmart Hopper";
