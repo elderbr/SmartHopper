83e9e9efb5f0136b949035fe803ace68c2857999
ElderBR <elderbarbosaromao@gmail.com>
2025-05-22 08:09:57 -0300

    Criado a interface para armarzenar os nomes dos titulos

---
+++ src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
@@ -0,0 +1,15 @@
+package mc.elderbr.smarthopper.interfaces;
+
+public interface ITitle {
+
+    String TITLE_ID_ITEM = "§e§lID: ";
+    String TITLE_NAME_ITEM = "§2§lItem: §6";
+    String TITLE_GROUP = "§lGrupo: §r";
+    String TITLE_ID_GROUP = "§6§lID: ";
+    String TITLE_NAME_GROUP = "§9§lGrupo: §e";
+    String TITLE_NEW_GROUP = "§lNovo Grupo: §r";
+
+    String NAME_SMART_HOPPER = "§f§lSmart Hopper";
+    String NAME_RECIPE = "§f§lSmart Hopper";
+
+}
