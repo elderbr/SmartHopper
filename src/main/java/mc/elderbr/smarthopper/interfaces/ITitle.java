684d296d1e19bf45cf7f4f4dc5f5f31cf8525a66
ElderBR <elderbarbosaromao@gmail.com>
2025-05-22 12:08:44 -0300

    ITitle: Adicionado o título para a receita

--- src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
+++ src/main/java/mc/elderbr/smarthopper/interfaces/ITitle.java
@@ -10,6 +10,7 @@ public interface ITitle {
     String TITLE_NEW_GROUP = "§lNovo Grupo: §r";
 
     String NAME_SMART_HOPPER = "§f§lSmart Hopper";
+    String TITLE_RECIPE = "§a§lSmart Hopper";
     String NAME_RECIPE = "§f§lSmart Hopper";
 
 }
