fca73d724465681c1ab947b07f1d0d72cd00b849
ElderBR <elderbarbosaromao@gmail.com>
2025-05-22 17:51:49 -0300

    ItemFactory: Criado o item funil de ferro do Smart Hopper

--- src/main/java/mc/elderbr/smarthopper/factories/ItemFactory.java
+++ src/main/java/mc/elderbr/smarthopper/factories/ItemFactory.java
@@ -1,13 +1,29 @@
 package mc.elderbr.smarthopper.factories;
 
+import mc.elderbr.smarthopper.interfaces.VGlobal;
 import mc.elderbr.smarthopper.model.Item;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-public class ItemFactory {
+import java.util.List;
 
-    public static Item vazio(){
+public class ItemFactory implements VGlobal {
+
+    public static Item vazio() {
         Item item = new Item();
         item.setId(0);
         item.setName("vazio");
+        return item;
+    }
+
+    public static ItemStack getIronHopper() {
+        ItemStack item = new ItemStack(Material.HOPPER);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(TITLE_RECIPE);
+        meta.setLore(List.of(NAME_RECIPE));
+        meta.setCustomModelData(14);
+        item.setItemMeta(meta);
         return item;
     }
 }
